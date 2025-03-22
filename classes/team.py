8645b5bd20176dfaf2277c7d9b013635bfa39be1
johnphillips114 <johnphillips114@gmail.com>
2025-03-22 07:35:10 -0600

    Clean whitespace when loading team locations and names

--- classes/team.py
+++ classes/team.py
@@ -1,6 +1,7 @@
 import random
 from definitions import ROOT_DIR
 
+
 def initialize_teams():
     # Create all teams at once, so that school locations don't get re-used.
     # Team mascost/names can be re-used, as happens irl
@@ -14,23 +15,22 @@ def initialize_teams():
         for line in team_name_file:
             available_names.append(line)
 
-    for i in range(0,64):
-        location_index = random.randint(0,len(available_schools)-1)
+    for i in range(0, 64):
+        location_index = random.randint(0, len(available_schools) - 1)
         location = available_schools.pop(location_index)
-        team_name = available_names[random.randint(0, len(available_names)-1)]
+        location = str.strip(location)
+        team_name = available_names[random.randint(0, len(available_names) - 1)]
+        team_name = str.strip(team_name)
         new_team = Team(location, team_name)
         teams_in_tournament.append(new_team)
-    return teams_in_tournament
 
+    return teams_in_tournament
 
 
 class Team:
     def __init__(self, school_location, school_team_name):
-        location = school_location
-        name = school_team_name
-    
+        self.location = school_location
+        self.name = school_team_name
+
     def __str__(self):
         return f"{self.location} {self.name}"
-    
-
-
