4eb33b8458ad290e9ddd65517617399e87067fba
johnphillips114 <johnphillips114@gmail.com>
2025-03-22 08:17:05 -0600

    set a seed parameter for Team

---
+++ classes/conference.py
@@ -0,0 +1,4 @@
+class Conference:
+    def __init__(self, label, teams):
+        self.label = label
+        self.teams = teams
--- classes/team.py
+++ classes/team.py
@@ -1,7 +1,8 @@
 class Team:
-    def __init__(self, school_location, school_team_name):
+    def __init__(self, school_location, school_team_name, seed=1):
         self.location = school_location
         self.name = school_team_name
+        self.seed = seed
 
     def __str__(self):
         return f"{self.location} {self.name}"
