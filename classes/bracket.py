49510f0dee94cb4b0a4b3f04008ccd1e08a18709
johnphillips114 <johnphillips114@gmail.com>
2025-03-22 08:05:01 -0600

    removed printf debugging from bracket.py

--- classes/bracket.py
+++ classes/bracket.py
@@ -1,12 +1,14 @@
+import logging
 import random
 from classes import team
 from definitions import ROOT_DIR
+
+logger = logging.getLogger(__name__)
 
 
 class Bracket:
     def __init__(self):
         self.teams = self.initialize_teams()
-
 
     def initialize_teams(self):
         # Create all teams at once, so that school locations don't get re-used.
@@ -14,7 +16,9 @@ class Bracket:
         teams_in_tournament = []
         available_schools = []
         available_names = []
-        with open(f"{ROOT_DIR}/data/school_location_names.txt", "r") as team_location_file:
+        with open(
+            f"{ROOT_DIR}/data/school_location_names.txt", "r"
+        ) as team_location_file:
             for line in team_location_file:
                 available_schools.append(line)
         with open(f"{ROOT_DIR}/data/school_team_names.txt", "r") as team_name_file:
--- test/test_bracket.py
+++ test/test_bracket.py
@@ -1,6 +1,14 @@
-from classes import team
+from classes.bracket import Bracket
 
 
 def test_initialize_teams():
-    assert len(team.initialize_teams()) == 64
+    bracket = Bracket()
+    assert len(bracket.teams) == 64
 
+
+def test_bracket_location_uniqueness():
+    bracket = Bracket()
+    locations = set()
+    for team in bracket.teams:
+        locations.add(team.location)
+    assert len(locations) == 64
