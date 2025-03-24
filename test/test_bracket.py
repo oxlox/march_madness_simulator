7452aa0a82ec776e54ba66bf7b5de97467461792
John Phillips <johnphillips114@gmail.com>
2025-03-23 19:17:30 -0600

    Added tests

--- test/test_bracket.py
+++ test/test_bracket.py
@@ -1,9 +1,32 @@
 from classes.bracket import Bracket
 
 
-
 def test_get_round_1_matchups():
     bracket = Bracket()
     bracket.build_matchups()
     matchups = bracket.get_round_1_matchups()
+    assert len(matchups) == 4
+
+
+def test_get_round_2_matchups():
+    bracket = Bracket()
+    bracket.build_matchups()
+    bracket.simulate_bracket()
+    matchups = bracket.get_round_2_matchups()
+    assert len(matchups) == 4
+
+
+def test_get_round_3_matchups():
+    bracket = Bracket()
+    bracket.build_matchups()
+    bracket.simulate_bracket()
+    matchups = bracket.get_round_3_matchups()
+    assert len(matchups) == 4
+
+
+def test_get_round_4_matchups():
+    bracket = Bracket()
+    bracket.build_matchups()
+    bracket.simulate_bracket()
+    matchups = bracket.get_round_4_matchups()
     assert len(matchups) == 4
