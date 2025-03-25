d7e1d5d23db4337728b483cfb662c26bd07a7e6b
John Phillips <johnphillips114@gmail.com>
2025-03-24 19:05:14 -0600

    run Conference Team location uniqueness test 20 times

--- test/test_conference.py
+++ test/test_conference.py
@@ -1,10 +1,12 @@
 from classes.conference import Conference, ConferenceName
 
 def test_team_location_uniqueness():
-    name = ConferenceName(1)
-    conference = Conference(name)
-    conference.initialize_teams()
-    team_locations = []
-    for team in conference.teams:
-        team_locations.append(team.location)
-    assert len(set(team_locations)) == 16
+    for i in range(0, 20):
+        name = ConferenceName(1)
+        conference = Conference(name)
+        conference.initialize_teams()
+        team_locations = []
+        for team in conference.teams:
+            team_locations.append(team.location)
+        assert len(set(team_locations)) == 16
+        
